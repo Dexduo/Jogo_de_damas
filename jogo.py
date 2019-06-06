24356d8a48f539883adb1f81c8430689aeeaab98
José Eduardo <eduardosoaresn3@gmail.com>
2019-06-05 22:55:04 -0300

    Novas ideias, não mexer por enquanto por favor

--- jogo.py
+++ jogo.py
@@ -19,28 +19,37 @@ tabuleiro(tab)
 coluna = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9}
 linha = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
 
-# comida_cima = 0
-# comida_baixa = 0
+qtd_cima = 15
+qtd_baixo = 15
 
-#while comida_baixa<=15 or comida_cima<=15:
+while qtd_cima != 0 or qtd_baixo !=0:
 
-jogador_vez = input("Quem vai jogar agora? ")
-jogada = input("Faça sua jogada: ")
+	jogador_vez = input("Quem vai jogar agora? ")
+	jogada = input("Faça sua jogada: ")
 
-col1 = coluna[jogada[0]]
-lin1 = linha[jogada[1]]
-col2 = coluna[jogada[4]]
-lin2 = linha[jogada[5]]
+	col1 = coluna[jogada[0]]
+	lin1 = linha[jogada[1]]
+	col2 = coluna[jogada[4]]
+	lin2 = linha[jogada[5]]
 
-if (tab[lin1][col1] == "@" or tab[lin1][col1] == "o"):
-	peca = "normal"
-else:
-	if tab[lin1][col1] == "&" or tab[lin1][col1] == "O":
-		peca = "dama"
+	if (tab[lin1][col1] == "@" or tab[lin1][col1] == "o"):
+		peca = "normal"
 	else:
-		peca = "invalida"
+		if tab[lin1][col1] == "&" or tab[lin1][col1] == "O":
+			peca = "dama"
+		else:
+			peca = "invalida"
 
-nova_pos(tab, peca, jogador_vez, lin1, col1, lin2, col2)
+	if qtd_cima == 0:
+		print("O vencedor é o usuário de Baixo")
+	if qtd_baixo == 0:
+		print("O vencedor é o usuário de Cima")
+
+
+
+
+	nova_pos(tab, peca, jogador_vez, lin1, col1, lin2, col2, qtd_cima, qtd_baixo)
+	tabuleiro(tab)
 
 
 
--- posicao.py
+++ posicao.py
@@ -1,9 +1,36 @@
-def nova_pos(tab, peca, jogador_vez, lin1, col1, lin2, col2):
+def nova_pos(tab, peca, jogador_vez, lin1, col1, lin2, col2, qtd_cima, qtd_baixo):
+
 	coluna = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9}
 	linha = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
 
+
+	if peca == "dama":
+
+		if jogador_vez == "B":
+			if lin2<lin1:
+				if col2<col1:
+					passo = col1
+					qtd_inimiga = 0
+					for i in range(lin1-1, lin2-1, -1):
+						passo-=1
+						if tab[i][passo] == "o":
+							print("pegou")
+							qtd_inimiga+=1
+							l_pego = i
+							c_pego = passo
+						else:
+							print("Nao pegou")
+					if qtd_inimiga == 1:
+						tab[l_pego][c_pego] = " "
+						qtd_baixo-=1
+						tab[lin2][col2] = "&"
+					else:
+						print("Jogada invalida")
+					
 	if peca == "normal":
+
 		if jogador_vez=="C":
+
 			if (lin1-lin2 >= 1 and (col1==col2+1 or col1==col2-1)) or (lin2>lin1+1) or (tab[lin2][col2] != " "):
 				print("Jogada invalida") 
 			else:
@@ -11,11 +38,77 @@ def nova_pos(tab, peca, jogador_vez, lin1, col1, lin2, col2):
 				tab[lin2][col2] = "o"
 
 		if jogador_vez=="B":
-			if (lin1-lin2 >= -1 and (col1==col2+1 or col1==col2-1)) or (lin1>lin2+1) or (tab[lin2][col2] != " "):
+
+			if (lin2-lin1 >= 1 and (col1==col2+1 or col1==col2-1)) or (lin1>lin2+1) or (tab[lin2][col2] != " "):
 				print("Jogada invalida") 
 			else:
 				tab[lin1][col1] = " "
 				tab[lin2][col2] = "@"
 
-		
+
+	# if peca == "dama":
+
+	# 	if tab[lin2][col2] != " ":
+	# 		print("Jogada invalida")
+
+	# 	if jogador_vez == "C":
+	# 		if lin2>lin1:
+	# 			if col2>col1:
+	# 				teste = col1
+	# 				peca_inimiga = 0
+	# 				for i in range(lin1+1, lin2+1):
+	# 					if tab[i][teste] == "o" or tab[i][teste] == "O":
+	# 						print("Jogada invalida")
+	# 					if tab[i][teste] == "@" or tab[i][teste] == "&":
+	# 						peca_inimiga+=1
+	# 					teste+=1
+	# 				if peca_inimiga>1:
+	# 					print("Jogada invalida")
+
+	# 			if col2<col1:
+	# 				teste = col1
+	# 				peca_inimiga = 0
+	# 				for i in range(lin1+1, lin2+1):
+	# 					if tab[i][teste] == "o" or tab[i][teste] == "O":
+	# 						print("Jogada invalida")
+	# 					if tab[i][teste] == "@" or tab[i][teste] == "&":
+	# 						peca_inimiga+=1
+	# 					teste-=1
+	# 				if peca_inimiga>1:
+	# 					print("Jogada invalida")
+
+	# 		if lin2<lin1:
+	# 			if col2>col1:
+	# 				teste = col1
+	# 				peca_inimiga = 0
+	# 				for i in range(lin1+1, lin2+2, -1):
+	# 					if tab[i][teste] == "o" or tab[i][teste] == "O":
+	# 						print("Jogada invalida")
+	# 					if tab[i][teste] == "@" or tab[i][teste] == "&":
+	# 						peca_inimiga+=1
+	# 					teste+=1
+	# 				if peca_inimiga>1:
+	# 					print("Jogada invalida")
+
+	# 			if col2<col1:
+	# 				teste = col1
+	# 				peca_inimiga = 0
+	# 				for i in range(lin1+1, lin2+2, -1):
+	# 					if tab[i][teste] == "o" or tab[i][teste] == "O":
+	# 						print("Jogada invalida")
+	# 					if tab[i][teste] == "@" or tab[i][teste] == "&":
+	# 						peca_inimiga+=1
+	# 					teste-=1
+	# 				if peca_inimiga>1:
+	# 					print("Jogada invalida")
+
+
+
+
+
+
+
+
+
+
 
