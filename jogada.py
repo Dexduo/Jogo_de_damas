27b2d73faefde914e6095e23a9af003c27685745
José Eduardo <eduardosoaresn3@gmail.com>
2019-06-14 22:11:15 -0300

    Alguns ajustes

--- jogada.py
+++ jogada.py
@@ -11,75 +11,81 @@ def posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada):
 
 	conf = 1
 	comer = 0
-	#PRIMEIRAMENTE IREI CHECAR SE A PEÇA É NORMAL
-	if peca == "normal":
-		#AQUI SE A PEÇA FOR NORMAL E TENTAR PULAR MAIS DE 2 CASAS É INVALIDA
-		if int(((lin2-lin1)**2)**(1/2))>2 or int(((col2-col1)**2)**(1/2))>2 or tab[lin2][col2] != " ":
-			print("Jogada Invalida")
-			jogada = input("Refaça sua jogada: ")
-			posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
-		else:
- 		#SE NÃO PULAR MAIS DE DUAS CASAS EU VEJO QUEM VAI JOGAR AGORA
- 			#SE QUEM JOGAR FOR AS NORMAIS DE BAIXO:
-			if jogador_vez == "B":
-				
-				comer = checar(tab, comer)
-				
-				lin_chance = int(((( (lin1)**2)**(1/2) ) + (( (lin2)**2)**(1/2) ))/2)
-				col_chance = int(((( (col1)**2)**(1/2) ) + (( (col2)**2)**(1/2) ))/2)
-				#SE TIVER ADVERSARIAS AO REDOR EU CHECO SE A JOGADA PASSOU POR CIMA DA PEÇA ADVERSÁRIA
-				if comer == 1:
-					if tab[lin_chance][col_chance] != "o" and tab[lin_chance][col_chance] != "O":
-						print("Jogada invalida")
-						jogada = input("Refaça sua jogada: ")
-						posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
-					else:
-						#SE ELA PASSOU POR CIMA (COMEU) EU ATUALIZO O TABULEIRO
-						tab[lin2][col2] = tab[lin1][col1]
-						tab[lin_chance][col_chance] = " "
-						tab[lin1][col1] = " "
 
-				#SE NÃO TIVER PEÇAS ADVERŚÁRIAS AO REDOR EU CHECO SE ELE TENTOU PULAR MAIS DE UMA CASA
-				else:
-					if (int(((lin2-lin1)**2)**(1/2))!=1 or int(((col2-col1)**2)**(1/2))!=1) or lin2>lin1:
-						print("Jogada Invalida")
-						jogada = input("Refaça sua jogada: ")
-						posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
+	if jogador_vez != "C" and jogador_vez != "B":
+		jogador_vez = input("Usuário invalido, quem vai jogar agora?: ")
+		posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
+
+	else:
+		#PRIMEIRAMENTE IREI CHECAR SE A PEÇA É NORMAL
+		if peca == "normal":
+			#AQUI SE A PEÇA FOR NORMAL E TENTAR PULAR MAIS DE 2 CASAS É INVALIDA
+			if int(((lin2-lin1)**2)**(1/2))>2 or int(((col2-col1)**2)**(1/2))>2 or tab[lin2][col2] != " ":
+				print("Jogada Invalida")
+				jogada = input("Refaça sua jogada: ")
+				posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
+			else:
+	 		#SE NÃO PULAR MAIS DE DUAS CASAS EU VEJO QUEM VAI JOGAR AGORA
+	 			#SE QUEM JOGAR FOR AS NORMAIS DE BAIXO:
+				if jogador_vez == "B":
+					
+					comer = checar(tab, comer)
+					
+					lin_chance = int(((( (lin1)**2)**(1/2) ) + (( (lin2)**2)**(1/2) ))/2)
+					col_chance = int(((( (col1)**2)**(1/2) ) + (( (col2)**2)**(1/2) ))/2)
+					#SE TIVER ADVERSARIAS AO REDOR EU CHECO SE A JOGADA PASSOU POR CIMA DA PEÇA ADVERSÁRIA
+					if comer == 1:
+						if tab[lin_chance][col_chance] != "o" and tab[lin_chance][col_chance] != "O":
+							print("Jogada invalida")
+							jogada = input("Refaça sua jogada: ")
+							posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
+						else:
+							#SE ELA PASSOU POR CIMA (COMEU) EU ATUALIZO O TABULEIRO
+							tab[lin2][col2] = tab[lin1][col1]
+							tab[lin_chance][col_chance] = " "
+							tab[lin1][col1] = " "
+
+					#SE NÃO TIVER PEÇAS ADVERŚÁRIAS AO REDOR EU CHECO SE ELE TENTOU PULAR MAIS DE UMA CASA
 					else:
-						#SE NÃO PULOU MAIS DE UMA CASA EU ATUALIZO O TABULEIRO
-						tab[lin2][col2] = tab[lin1][col1]
-						tab[lin1][col1] = " "
+						if (int(((lin2-lin1)**2)**(1/2))!=1 or int(((col2-col1)**2)**(1/2))!=1) or lin2>lin1:
+							print("Jogada Invalida")
+							jogada = input("Refaça sua jogada: ")
+							posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
+						else:
+							#SE NÃO PULOU MAIS DE UMA CASA EU ATUALIZO O TABULEIRO
+							tab[lin2][col2] = tab[lin1][col1]
+							tab[lin1][col1] = " "
 
 
-			#SE QUEM JOGAR FOR AS NORMAIS DE CIMA:
-			if jogador_vez == "C":
-				
-				comer = checar(tab, comer)
-				
-				lin_chance = int(((( (lin1)**2)**(1/2) ) + (( (lin2)**2)**(1/2) ))/2)
-				col_chance = int(((( (col1)**2)**(1/2) ) + (( (col2)**2)**(1/2) ))/2)
-				#SE TIVER ADVERSARIAS AO REDOR EU CHECO SE A JOGADA PASSOU POR CIMA DA PEÇA ADVERSÁRIA
-				if comer == 1:
-					if tab[lin_chance][col_chance] != "@" and tab[lin_chance][col_chance] != "&":
-						print("Jogada invalida")
-						jogada = input("Refaça sua jogada: ")
-						posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
-					else:
-						#SE ELA PASSOU POR CIMA (COMEU) EU ATUALIZO O TABULEIRO
-						tab[lin2][col2] = tab[lin1][col1]
-						tab[lin_chance][col_chance] = " "
-						tab[lin1][col1] = " "
+				#SE QUEM JOGAR FOR AS NORMAIS DE CIMA:
+				if jogador_vez == "C":
+					
+					comer = checar(tab, comer)
+					
+					lin_chance = int(((( (lin1)**2)**(1/2) ) + (( (lin2)**2)**(1/2) ))/2)
+					col_chance = int(((( (col1)**2)**(1/2) ) + (( (col2)**2)**(1/2) ))/2)
+					#SE TIVER ADVERSARIAS AO REDOR EU CHECO SE A JOGADA PASSOU POR CIMA DA PEÇA ADVERSÁRIA
+					if comer == 1:
+						if tab[lin_chance][col_chance] != "@" and tab[lin_chance][col_chance] != "&":
+							print("Jogada invalida")
+							jogada = input("Refaça sua jogada: ")
+							posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
+						else:
+							#SE ELA PASSOU POR CIMA (COMEU) EU ATUALIZO O TABULEIRO
+							tab[lin2][col2] = tab[lin1][col1]
+							tab[lin_chance][col_chance] = " "
+							tab[lin1][col1] = " "
 
-				#SE NÃO TIVER PEÇAS ADVERŚÁRIAS AO REDOR EU CHECO SE ELE TENTOU PULAR MAIS DE UMA CASA
-				else:
-					if (int(((lin2-lin1)**2)**(1/2))!=1 or int(((col2-col1)**2)**(1/2))!=1) or lin2<lin1:
-						print("Jogada Invalida")
-						jogada = input("Refaça sua jogada: ")
-						posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
+					#SE NÃO TIVER PEÇAS ADVERŚÁRIAS AO REDOR EU CHECO SE ELE TENTOU PULAR MAIS DE UMA CASA
 					else:
-						#SE NÃO PULOU MAIS DE UMA CASA EU ATUALIZO O TABULEIRO
-						tab[lin2][col2] = tab[lin1][col1]
-						tab[lin1][col1] = " "
+						if (int(((lin2-lin1)**2)**(1/2))!=1 or int(((col2-col1)**2)**(1/2))!=1) or lin2<lin1:
+							print("Jogada Invalida")
+							jogada = input("Refaça sua jogada: ")
+							posicao(peca, lin1, col1, lin2, col2, tab, jogador_vez, jogada)
+						else:
+							#SE NÃO PULOU MAIS DE UMA CASA EU ATUALIZO O TABULEIRO
+							tab[lin2][col2] = tab[lin1][col1]
+							tab[lin1][col1] = " "
 
 
 
